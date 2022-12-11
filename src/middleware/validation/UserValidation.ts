7a18bf24af7cba97eb35f08ff520fc1765d603c4
Tomi Prasetyo <tomi.prasetyo95@gmail.com>
2022-12-11 14:19:40 +0700

    User Registration & Input Validation

---
+++ src/middleware/validation/UserValidation.ts
@@ -0,0 +1,62 @@
+import Validator from "validatorjs";
+import { Request, Response, NextFunction } from "express";
+import Helper from "../../helpers/Helper";
+import User from "../../db/models/User";
+
+const RegisterValidation = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { name, email, password, confirmPassword } = req.body;
+
+    const data = {
+      name,
+      email,
+      password,
+      confirmPassword,
+    };
+
+    const rules: Validator.Rules = {
+      name: "required|string|max:50",
+      email: "required|email",
+      password: "required|min:8",
+      confirmPassword: "required|same:password",
+    };
+
+    // compare
+    const validate = new Validator(data, rules);
+
+    if (validate.fails()) {
+      return res
+        .status(400)
+        .send(Helper.ResponseData(400, "Bad Request", validate.errors, null));
+    }
+
+    const user = await User.findOne({
+      where: {
+        email: email,
+      },
+    });
+
+    if (user) {
+      const errorData = {
+        errors: {
+          email: ["Email Already Been Used"],
+        },
+      };
+      return res
+        .status(400)
+        .send(Helper.ResponseData(400, "Bad Request", errorData, null));
+    }
+
+    next();
+  } catch (error: any) {
+    return res
+      .status(500)
+      .send(Helper.ResponseData(500, "Bad Request", error, null));
+  }
+};
+
+export default { RegisterValidation };
--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,6 +1,8 @@
 import express from "express";
 
 import RoleController from "../controllers/RoleController";
+import UserController from "../controllers/UserController";
+import UserValidation from "../middleware/validation/UserValidation";
 
 const router = express.Router();
 
@@ -9,5 +11,12 @@ router.post("/role", RoleController.CreateRole);
 router.post("/role/:id", RoleController.UpdateRole);
 router.delete("/role/:id", RoleController.DeleteRole);
 router.get("/role/:id", RoleController.GetRoleById);
+
+// User Routing
+router.post(
+  "/user/signup",
+  UserValidation.RegisterValidation,
+  UserController.Register
+);
 
 export default router;
